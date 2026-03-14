8fdb5194dd46ae4acddaeafc0f158ca59792d79e
sagar <sgr@duck.com>
2026-03-14 13:21:07 +0530

    JWT Implementation

--- src/main/java/com/sgr9/url_shortener/models/UrlMapping.java
+++ src/main/java/com/sgr9/url_shortener/models/UrlMapping.java
@@ -24,3 +24,4 @@ public class UrlMapping {
     @OneToMany(mappedBy = "urlMapping")
     private List<ClickEvent> clickEvents;
 }
+  
---
+++ src/main/java/com/sgr9/url_shortener/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sgr9.url_shortener.repository;
+
+
+import com.sgr9.url_shortener.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
